3ea3cca86dbe0ad0e35aa20f5b44b1a9c8d8351a
Opal <guerre5lily@gmail.com>
2021-11-06 19:52:12 -0700

    add block colors, save data to local storage

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Work Day Scheduler Starter Code
+
+# Work Day Planner:
+* when entering in the text field to plan their day, one will push the SAVE button. text will save to local storage and will persist beyond a refresh
+* time blocks are color coded and will appear different colors depending on the time of day
+
+
+github repo: https://github.com/lsguerre/workday-planner
+deployed: https://lsguerre.github.io/workday-planner/
+
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,51 @@
 //display current day on page
-$("#currentDay").text("It's " + moment().format("dddd, MMMM Do YYYY"))
+$("#currentDay").text("It's " + moment().format("dddd, MMMM Do YYYY") + "!")
+
+
 
 var saveBtn = $(".saveBtn")
 
-$(saveBtn).on("click", function () {
-    var time = $(this).siblings(".hour").text().trim()
-    var gamePlan = $(this).siblings(".work-input").text().trim()
 
-    localStorage.setItem(time, gamePlan)
+$(saveBtn).on("click", function () {
+        
+    var time = $(this).parent().attr("id")
+    var workInput = $(this).siblings(".input").val()
+
+    localStorage.setItem(time, workInput)
     console.log(localStorage)
+
 })
+
+$("#8am .input").val(localStorage.getItem("8am"))
+$("#9am .input").val(localStorage.getItem("9am"))
+$("#10am .input").val(localStorage.getItem("10am"))
+$("#11am .input").val(localStorage.getItem("11am"))
+$("#12pm .input").val(localStorage.getItem("12pm"))
+$("#1pm .input").val(localStorage.getItem("1pm"))
+$("#2pm .input").val(localStorage.getItem("2pm"))
+$("#3pm .input").val(localStorage.getItem("3pm"))
+$("#4pm .input").val(localStorage.getItem("4pm"))
+$("#5pm .input").val(localStorage.getItem("5pm"))
+
+function blockColor() {
+    var hour = moment().hours()
+
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"))
+
+        if(currentHour > hour) {
+            $(this).addClass("future")
+
+        } else if(currentHour === hour) {
+            $(this).addClass("present")
+
+        } else {
+            $(this).addClass("past")
+        }
+        
+
+    })
+}
+
+blockColor()
+console.log(blockColor)
